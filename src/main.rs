94d09affbfb8a46441658fa466c6d358fa8a52a7
8845musign <8845musign@gmail.com>
2017-05-27 11:34:23 +0900

    Imple get user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+
+    println!("Please inpu your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Fail to read line");
+
+    println!("You guessed: {}", guess);
 }
